389dd2f1bbbd0c3e3362020708cbc5565dde69b8
Joe Querin <joecue@gmail.com>
2016-08-31 10:43:14 -0400

    Commented out Style and JavaScript Files
    admin.css and admin.js are currently empty
    public.css and public.js are currently empty

--- admin/class-lorainccc-site-specific-admin.php
+++ admin/class-lorainccc-site-specific-admin.php
@@ -73,7 +73,8 @@ class Lorainccc_Site_Specific_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/lorainccc-site-specific-admin.css', array(), $this->version, 'all' );
+		//Commenting out since the files are empty.
+  //wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/lorainccc-site-specific-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +97,8 @@ class Lorainccc_Site_Specific_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/lorainccc-site-specific-admin.js', array( 'jquery' ), $this->version, false );
+		//Commenting out since the files are empty.
+  //wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/lorainccc-site-specific-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
